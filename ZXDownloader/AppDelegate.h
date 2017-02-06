538a7d3091bae55aefeec603aee98e92307693c3
sajiner <739134362@qq.com>
2017-02-06 14:00:24 +0800

    first commit

---
+++ ZXDownloader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXDownloader
+//
+//  Created by sajiner on 2017/2/6.
+//  Copyright © 2017年 sajiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZXDownloader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXDownloader
+//
+//  Created by sajiner on 2017/2/6.
+//  Copyright © 2017年 sajiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
